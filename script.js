101ce3fb7c66cd350c0da5bc35c1a4e04bf23ebf
Kriepp <113834453+Kriepp@users.noreply.github.com>
2024-02-15 18:42:52 +0200

    Added the gameover
    Added the function for the gameover add the confirm to chose to continue or not

--- script.js
+++ script.js
@@ -1,72 +1,108 @@
-const playerChar = document.getElementById("player")
-const setEnemy = document.getElementById("enemy")
-let gameOn = false
-let enemyTypes = ["enemy1", "enemy2", "enemy3"]
-
+const playerChar = document.getElementById('player');
+const setEnemy = document.getElementById('enemy');
+let gameOn = false;
+let enemyTypes = ['enemy1', 'enemy2', 'enemy3'];
 
 const spawn_enemy = () => {
-    let pickEnemy = enemyTypes[(Math.floor(Math.random() * (enemyTypes.length)))]
-    if (gameOn == true) {
-        setEnemy.classList.add(pickEnemy)
-        setTimeout(function(){
-            setEnemy.classList.remove(pickEnemy)
-            }, 3000)
-    }
-
-}
-
-
+  let pickEnemy = enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
+  if (gameOn == true) {
+    setEnemy.classList.add(pickEnemy);
+    setTimeout(function () {
+      setEnemy.classList.remove(pickEnemy);
+    }, 3000);
+  }
+};
 
 const game_start = () => {
-    if (gameOn == false) {
-        console.log("Game On")
-        gameOn = true
+  if (gameOn == false) {
+    console.log('Game On');
+    gameOn = true;
+  }
+};
 
-    }
+// Game over
+function checkGameOver() {
+  const playerAvatar = playerChar.getBoundingClientRect();
+  const enemyAvatar = setEnemy.getBoundingClientRect();
+
+  const gameOver = !(
+    playerAvatar.right < enemyAvatar.left ||
+    playerAvatar.left > enemyAvatar.right ||
+    playerAvatar.bottom < enemyAvatar.top ||
+    playerAvatar.top > enemyAvatar.bottom
+  );
+
+  let replay = null;
+
+  if (gameOver) {
+    replay = confirm('You lost!! Do you want to replay the game?');
+    gameOn = false;
+    clearInterval(gameInterval);
+    startColision();
+  }
+
+  if (replay) {
+    gameOn = true;
+    startColision();
+  }
 }
 
+function startColision() {
+  gameInterval = setInterval(checkGameOver, 100);
+}
+
+startColision();
 
 const jump = () => {
-    if (!playerChar.classList.contains("jump") && !playerChar.classList.contains("duck")){
-        playerChar.classList.add("jump")
-        setTimeout(function(){
-            playerChar.classList.remove("jump")
-            }, 500)
-    }
-}
+  if (
+    !playerChar.classList.contains('jump') &&
+    !playerChar.classList.contains('duck')
+  ) {
+    playerChar.classList.add('jump');
+    setTimeout(function () {
+      playerChar.classList.remove('jump');
+    }, 500);
+  }
+};
 
 const duck = () => {
-    if (!playerChar.classList.contains("duck") && !playerChar.classList.contains("jump")){
-        playerChar.classList.add("duck")
-        setTimeout(function(){
-            playerChar.classList.remove("duck")
-            }, 500)
-    }
-}
+  if (
+    !playerChar.classList.contains('duck') &&
+    !playerChar.classList.contains('jump')
+  ) {
+    playerChar.classList.add('duck');
+    setTimeout(function () {
+      playerChar.classList.remove('duck');
+    }, 500);
+  }
+};
 
 const spawn_timer = () => {
-   const min = 3001
-   const max = 5000
-   const random_time = (Math.floor(Math.random() * (max - min)) + min)
-    setInterval(spawn_enemy, random_time)
-}
+  const min = 3001;
+  const max = 5000;
+  const random_time = Math.floor(Math.random() * (max - min)) + min;
+  setInterval(spawn_enemy, random_time);
+};
 
-spawn_timer()
+spawn_timer();
 
-addEventListener("keydown", (event) => {
-    if (event.code == "Space"  && gameOn == true || event.code == "ArrowUp"  && gameOn == true) {
-        jump()
-    }
-})
+addEventListener('keydown', (event) => {
+  if (
+    (event.code == 'Space' && gameOn == true) ||
+    (event.code == 'ArrowUp' && gameOn == true)
+  ) {
+    jump();
+  }
+});
 
-addEventListener("keydown", (event) => {
-    if (event.code == "ArrowDown" && gameOn == true) {
-        duck()
-    }
-})
+addEventListener('keydown', (event) => {
+  if (event.code == 'ArrowDown' && gameOn == true) {
+    duck();
+  }
+});
 
-addEventListener("keydown", (event) => {
-    if (event.code == "Space" ) {
-        game_start()
-    }
-})
+addEventListener('keydown', (event) => {
+  if (event.code == 'Space') {
+    game_start();
+  }
+});
